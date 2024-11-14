591d0b0fb8608e3fcbcadb722cc82e30e60599f6
hannahazan <hannahazan2@gmail.com>
2024-11-14 11:05:08 +0100

    reactiv-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ArticleComponent } from './article/article.component';
+import { ArticleComponent } from './component/article/article.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,33 +11,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  articles = [
-    { 
-      title: 'Choisir Carapuce', 
-      author: 'Alice', 
-      content: 'Ce Pokémon crache une écume redoutable. Après sa naissance, son dos gonfle et durcit pour former une carapace.', 
-      image: 'assets/images/carapuce.png',
-      isPublished: true, 
-      comment: '', 
-      likes: 120 
-    },
-    { 
-      title: 'Choisir Bulbizarre', 
-      author: 'Bob', 
-      content: 'Au début de sa vie, il se nourrit des nutriments accumulés dans la graine sur son dos. Cela lui permet de grandir.', 
-      image: 'assets/images/Bulbizarre.png',
-      isPublished: false, 
-      comment: '', 
-      likes: 75 
-    },
-    { 
-      title: 'Choisir Salamèche', 
-      author: 'Charlie', 
-      content: 'La flamme au bout de sa queue représente sa vitalité. Quand Salamèche n’est pas au meilleur de sa forme, elle faiblit.', 
-      image: 'assets/images/salamèche.png',
-      isPublished: true, 
-      comment: '', 
-      likes: 200 
-    }
-  ];
+ 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ArticlePageComponent } from './article-page/article-page.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { ArticlePageComponent } from './component/article-page/article-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactFormComponent } from './component/contact-form/contact-form.component';
+import { ArticleComponent } from './component/article/article.component';
+import { SignupPageComponent } from './page/signup-page/signup-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent },
+    {path:'signup', component:SignupPageComponent },
+    {path:'articleTest', component:ArticleComponent},
     {path:'article/:id',component: ArticlePageComponent},
     {path:'contact',component:ContactFormComponent},
-    { path: '**', component: NotFoundComponent },
+    {path: '**', component: NotFoundComponent },
     
 ];
