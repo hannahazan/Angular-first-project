25ceb4addc3b95fbc606a1aa25bb50aa37a21ac0
hannahazan <hannahazan2@gmail.com>
2024-11-14 13:59:09 +0100

    refacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ArticlePageComponent } from './page/article-page/article-page.component';
+import { ArticleComponent } from './component/article/article.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,ArticlePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
