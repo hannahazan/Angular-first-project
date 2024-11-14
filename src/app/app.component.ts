aa9798cebfdbcef0536b17cd14d4d1ed39bbf72f
hannahazan <hannahazan2@gmail.com>
2024-11-14 12:40:56 +0100

    ignore this commit for correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ArticleComponent } from './component/article/article.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ArticleComponent,CommonModule],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
-import { ArticlePageComponent } from './component/article-page/article-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { ArticlePageComponent } from './page/article-page/article-page.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 import { ContactFormComponent } from './component/contact-form/contact-form.component';
-import { ArticleComponent } from './component/article/article.component';
 import { SignupPageComponent } from './page/signup-page/signup-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent },
     {path:'signup', component:SignupPageComponent },
-    {path:'articleTest', component:ArticleComponent},
     {path:'article/:id',component: ArticlePageComponent},
     {path:'contact',component:ContactFormComponent},
     {path: '**', component: NotFoundComponent },
